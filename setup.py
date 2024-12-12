82750260b178df56d97fce2a52ce774b78b196e9
trannhatdang <77575859+trannhatdang@users.noreply.github.com>
2024-12-13 00:32:17 +0700

    dfd

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="gym_cutting_stock",
+    name="deng_gym_cutting_stock",
     version="0.0.1",
     install_requires=["gymnasium", "pygame", "numpy", "matplotlib"],
 )
